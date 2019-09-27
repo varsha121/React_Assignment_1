6382257b9c9c7b513f65f6d60a8fee400dfd0e8d
varsha121 <varsha.k1199@gmail.com>
2019-09-26 20:48:10 -0400

    commit

---
+++ ReactAssignment_1/src/Strengthcrieteria.js
@@ -0,0 +1,12 @@
+import React from 'react';
+const Strengthcriteria=()=>{
+    return(
+    <div className="strengthcriteria">
+        <div>Minimum 8 charecters long</div>
+        <div>Atleast 1 capital letter</div>
+        <div>Atleast 1 number</div>
+        <div>Atleast 1 special charecter</div>
+    </div>
+    )
+}
+export default Strengthcriteria;
